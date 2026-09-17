a02621b34fd9b80cb0a120ff8cea4a33c655f22b
marionvayt <vusatko@gmail.com>
2026-09-17 22:45:11 +0300

    Implement room deletion endpoint

--- ConferenceBookingApi/Controllers/RoomsController.cs
+++ ConferenceBookingApi/Controllers/RoomsController.cs
@@ -19,4 +19,46 @@ public class RoomsController : ControllerBase
     {
         return Ok(_rooms);
     }
+    
+    [HttpPost]
+    public IActionResult CreateRoom(Room newRoom)
+    {
+        int newId = _rooms.Any() ? _rooms.Max(r => r.Id) + 1 : 1;
+        newRoom.Id = newId;
+        
+        _rooms.Add(newRoom);
+        
+        return Ok(new { Message = "Зал успішно створено", RoomId = newId });
+    }
+    
+    [HttpPut("{id}")]
+    public IActionResult UpdateRoom(int id, Room updatedRoom)
+    {
+
+        var existingRoom = _rooms.FirstOrDefault(r => r.Id == id);
+        
+        if (existingRoom == null)
+        {
+            return NotFound(new { Message = "Зал з таким ID не знайдено" });
+        }
+        
+        existingRoom.Name = updatedRoom.Name;
+        existingRoom.Capacity = updatedRoom.Capacity;
+        existingRoom.BasePricePerHour = updatedRoom.BasePricePerHour;
+
+        return Ok(new { Message = "Інформацію про зал успішно оновлено" });
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteRoom(int id)
+    {
+        var existingRoom = _rooms.FirstOrDefault(r => r.Id == id);
+        if (existingRoom == null)
+        {
+            return NotFound(new { Message = "Зал з таким ID не знайдено" });
+        }
+        
+        _rooms.Remove(existingRoom);
+        return Ok(new {Message = "Зал успішно видалено"});
+    }
 }
